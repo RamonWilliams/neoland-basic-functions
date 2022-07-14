504d96d078d3f09b0ed3a00a5bf12f5d7b8afc75
Ramon Williams <ramnwor21@gmail.com>
2022-07-14 16:27:03 +0200

    Iterrcaión1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # neoland-basic-functions
+Completa la función que tomando dos números como argumento devuelva el más alto.
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne , numberTwo) {
+  // insert code
+}
---
+++ app.js
@@ -0,0 +1,10 @@
+  //Completa la función que tomando dos números como argumento devuelva el más alto.
+  //function sum(numberOne , numberTwo) {
+   // ITERACION 1
+  
+  const sum=(numberOne, numberTwo)=>{
+     let hiestNumber=numberOne>numberTwo? numberOne :  numberTwo;
+     return hiestNumber;
+  }
+  sum(1,2);
+  console.log(sum(1,2));
